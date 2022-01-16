21c2310541a1d7d76d5a54d3e8e5d0ee9236f620
Aiyush Gupta <55917505+Aiyush-G@users.noreply.github.com>
2022-01-16 19:04:53 +0000

    Added Additional Styles

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import './index.css'
+import './index.scss'
 
 createApp(App).use(router).use(store).mount('#app')
